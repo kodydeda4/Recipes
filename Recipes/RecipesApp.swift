0cce8aaabd9fbf51077bfa75d697ddc3a0219351
Kody Deda <kodydeda4@gmail.com>
2023-11-22 09:43:39 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Kody Deda on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
